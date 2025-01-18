dfbde397abd2ffab3502dc6060ea0c2b579a3e50
shoutrix <dasshoutrik@gmail.com>
2025-01-19 01:24:51 +0530

    add a few more hyperparams

--- train.py
+++ train.py
@@ -1,151 +1,72 @@
 import os
+import torch
 from data_utils import prepare_datasets, collate_fn, SortedSampler
 from E2asr_model import ASRconfig, E2ASR
 from torch.utils.data import DataLoader
-import torch
-import time
-import random
-import numpy as np
-from tqdm import tqdm
-import sys
-import torch.nn as nn
+from trainer import Trainer, CosineScheduler
 
 
-def set_seed(seed):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic=True
-    torch.backends.cudnn.benchmark=False
-
-set_seed(42)
-
-torch.set_num_threads(8)
-
-batch_size = 64
-max_frames = 38400
-
-train_set_name = "train"
-valid_set_name= "dev_clean"
-
+# HYPER PARAMETERS
 data_path = "/speech/shoutrik/torch_exp/E2asr/data/LibriTTS"
-expdir = "/speech/shoutrik/torch_exp/E2asr/exp"
-config = ASRconfig()
+expdir = "/speech/shoutrik/torch_exp/E2asr/exp/trial01"
+train_set_name = "train"
+valid_set_name = "dev_clean"
+max_frames = 38400
+batch_size = 64
+max_epoch = 30
+grad_norm_threshold = 1.0
+save_last_step_freq = 100
+save_global_step_freq = 50000
+logging_freq = 100
+grad_norm_threshold=1.0
+seed=42
+accum_grad=2
+config = ASRconfig(
+    sample_rate= 16000,
+    n_fft=512,
+    win_length=400,
+    hop_length=160,
+    n_mels=80,
+    center=True,
+    time_mask_param=30,
+    freq_mask_param=15,
+    norm_mean=True,
+    norm_var=True,
+    model_dim=512,
+    feedforward_dim=208,
+    dropout=0.1,
+    num_heads=8,
+    num_layers=18,
+    max_len=4992,
+)
 
-print("preparing datasets ...")
+
 train_dataset, valid_dataset, stoi, itos, sp = prepare_datasets(data_path, train_set_name, valid_set_name, expdir)
-
-
 train_sampler = SortedSampler(train_dataset, max_frames, batch_size, seed=42, stft_center=config.center, win_length=config.win_length, hop_length=config.hop_length)
 valid_sampler = SortedSampler(valid_dataset, max_frames, batch_size, seed=42, stft_center=config.center, win_length=config.win_length, hop_length=config.hop_length)
-
 train_loader = DataLoader(train_dataset, batch_sampler=train_sampler, collate_fn=collate_fn, num_workers=1, pin_memory=True)
 valid_loader = DataLoader(valid_dataset, batch_sampler=valid_sampler, collate_fn=collate_fn, num_workers=1, pin_memory=True)
 
 
 vocab_size = len(stoi) + 1
-# vocab_size = 96
-print("vocab_size : ", vocab_size)
-print(stoi)
-
-device = "cpu"
-if torch.cuda.is_available():
-    device = "cuda"
-print("using device : ", device)
-
 model = E2ASR(config, vocab_size, training=True)
-
-model = model.to(device)
-
-print(model)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
 
 
-def compute_grad_norm(model):
-    norm_type = 2.0
-    total_grad_norm = 0.0
-    high_grad_norm_modules = []
-    for name, param in model.named_parameters():
-        if param.grad is not None:
-            grad_norm = param.grad.norm(norm_type).item()
-            grad_norm = grad_norm ** norm_type
-            total_grad_norm += grad_norm
-            if grad_norm > grad_norm_threshold:
-                high_grad_norm_modules.append(f"{name} {grad_norm:.4f}")
-    total_grad_norm = total_grad_norm ** (1 / norm_type)
-    return total_grad_norm, high_grad_norm_modules
-    
+trainer = Trainer(model=model,
+                  train_loader=train_loader,
+                  valid_loader=valid_loader,
+                  device=device,
+                  expdir=expdir,
+                  accum_grad=accum_grad,
+                  max_epoch=max_epoch,
+                  save_last_step_freq=save_last_step_freq,
+                  save_global_step_freq=save_global_step_freq,
+                  resume=None,
+                  logging_freq=logging_freq,
+                  grad_norm_threshold=grad_norm_threshold,
+                  seed=seed
+                  )
 
-
-class WarmupScheduler:
-    def __init__(self, optimizer, lr, warmup_steps):
-        self.optimizer = optimizer
-        self.lr = lr
-        self.warmup_steps = warmup_steps
-    
-    def step(self):
-        lr_step = lr
-        for param_group in self.optimizer.param_groups:
-            param_group["lr"] = lr_step
-        
-        
-
-max_epoch = 30
-grad_norm_threshold = 1.0
-step = 0
-lr = 1e-5
-optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
-lr_scheduler = WarmupScheduler(optimizer, lr = lr, warmup_steps=10000)
-
-for epoch in range(max_epoch):
-    epoch_loss = 0
-    for i, batch in enumerate(train_loader):
-
-        batch_start_time = time.time()
-        speech = batch["speech"]
-        speech_lengths = batch["lengths"]
-        y = batch["tokens"]
-        speech = speech.to(device)
-        
-        speech, speech_lengths, y = speech.to(device), speech_lengths.to(device), y.to(device)
-        logits, loss, acc = model(speech, speech_lengths, y)
-
-        optimizer.zero_grad()
-        loss.backward()
-        
-        grad_norm, high_grad_norm_modules = compute_grad_norm(model)
-        print("grad norm : ", grad_norm)
-        # print(f"modules with gradient higher than {grad_norm_threshold} ----->")
-        # print(high_grad_norm_modules) 
-        
-        norm = torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-        lr_scheduler.step()
-        optimizer.step()
-        step += 1
-        batch_end_time = time.time()
-        n_frames = speech.shape[0]*speech.shape[1]
-        print(f"batch : {i+1}/{len(train_loader)} | loss: {loss.item():.4f} | acc : {acc*100:.4f}% | throughput : {int(n_frames / (batch_end_time-batch_start_time))} frames/sec")
-        epoch_loss += loss
-    #     break
-    # break
-        
-    epoch_loss = epoch_loss / (i+1)
-    print(f"epoch : {epoch}/{max_epoch} | loss : {epoch_loss}")
-
-    # validation
-    print("validating ...")
-    valid_loss = 0
-    
-    with torch.no_grad():
-        model.eval()
-        for batch in valid_loader:
-            speech = batch["speech"]
-            y = batch["tokens"]
-            speech_lengths = batch["lengths"]
-            speech, speech_lengths, y = speech.to(device), speech_lengths.to(device), y.to(device)
-            logits, loss, acc = model(speech, speech_lengths, y)
-            valid_loss += loss.item()
-        
-        valid_loss = valid_loss / len(valid_loader)
-        print(f"epoch : {epoch}/{max_epoch} | valid loss : {valid_loss}")
+trainer.train()
--- trainer.py
+++ trainer.py
@@ -5,8 +5,6 @@ import random
 import numpy as np
 from collections import defaultdict
 import math
-from torch.utils.data import DataLoader
-from tqdm import tqdm
 
 class CheckpointManager:
     def __init__(self, expdir, metric):
@@ -15,31 +13,34 @@ class CheckpointManager:
         self.param = metric
 
     def save_checkpoint(self, model, optimizer, lr_scheduler, step, metrics, last_step=True):
-        save_best = False
-        param_data = metrics[self.param]
-        best_ckpt_path = os.path.join(self.ckpt_dir, f"checkpoint_{self.param}_best.pt")
-
-        if len(param_data) > 1:
-            if (self.param.endswith("acc") and param_data[-1] > param_data[-2]) or \
-               (self.param.endswith("loss") and param_data[-1] < param_data[-2]):
-                save_best = True
-        else:
-            save_best = True
-
-        if save_best:
-            if os.path.exists(best_ckpt_path):
-                os.remove(best_ckpt_path)
-            self.save_(model, optimizer, lr_scheduler, step, metrics, best_ckpt_path)
-
+        
         if last_step:
+            save_best = False
+            param_data = metrics[self.param]
+            best_ckpt_path = os.path.join(self.ckpt_dir, f"checkpoint_{self.param}_best.pt")
+
+            if len(param_data) > 1:
+                if (self.param.endswith("acc") and param_data[-1] > param_data[-2]) or \
+                (self.param.endswith("loss") and param_data[-1] < param_data[-2]):
+                    save_best = True
+            else:
+                save_best = True
+
+            if save_best:
+                if os.path.exists(best_ckpt_path):
+                    os.remove(best_ckpt_path)
+                self.save_(model, optimizer, lr_scheduler, step, metrics, best_ckpt_path)
+
+
             last_ckpt_path = os.path.join(self.ckpt_dir, "checkpoint_last.pt")
             if os.path.exists(last_ckpt_path):
                 os.remove(last_ckpt_path)
             ckpt_path = last_ckpt_path
+            self.save_(model, optimizer, lr_scheduler, step, metrics, ckpt_path)
+        
         else:
             ckpt_path = os.path.join(self.ckpt_dir, f"checkpoint_step_{step}.pt")
-
-        self.save_(model, optimizer, lr_scheduler, step, metrics, ckpt_path)
+            self.save_(model, optimizer, lr_scheduler, step, metrics, ckpt_path)
 
     def save_(self, model, optimizer, lr_scheduler, step, metrics, ckpt_path):
         checkpoint = {
@@ -52,18 +53,23 @@ class CheckpointManager:
         torch.save(checkpoint, ckpt_path)
         print(f"Checkpoint saved to {ckpt_path}")
 
-
-    def load_checkpoint(self, ckpt_path, model, optimizer=None, lr_scheduler=None):
+    def load_(self, ckpt_path, model, optimizer=None, lr_scheduler=None):
         if not os.path.exists(ckpt_path):
             print(f"Checkpoint path {ckpt_path} does not exist.")
             return None
         checkpoint = torch.load(ckpt_path)
         model.load_state_dict(checkpoint['model_state_dict'])
+        
         if optimizer is not None:
             optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         if lr_scheduler is not None:
             lr_scheduler.load_state_dict(checkpoint['lr_scheduler_state_dict'])
-        return checkpoint['step'], checkpoint['metrics']
+        
+        step = checkpoint['step']
+        metrics = checkpoint['metrics']
+        
+        print(f"Checkpoint loaded from {ckpt_path}")
+        return model, optimizer, lr_scheduler, step, metrics
 
 
 class CosineScheduler:
@@ -84,10 +90,24 @@ class CosineScheduler:
         self.current_step += 1
         return self.get_lr()
 
+    def state_dict(self):
+        return {
+            'base_lr': self.base_lr,
+            'warmup_steps': self.warmup_steps,
+            'total_steps': self.total_steps,
+            'current_step': self.current_step
+        }
+
+    def load_state_dict(self, state_dict):
+        self.base_lr = state_dict['base_lr']
+        self.warmup_steps = state_dict['warmup_steps']
+        self.total_steps = state_dict['total_steps']
+        self.current_step = state_dict['current_step']
+
+
 
 class Trainer:
-    def __init__(self, model, train_loader, valid_loader, device, expdir, accum_grad, max_epoch, grad_norm_threshold, save_last_step_freq, save_global_step_freq):
-        self.model = model
+    def __init__(self, model, train_loader, valid_loader, device, expdir, accum_grad, max_epoch, grad_norm_threshold, save_last_step_freq, save_global_step_freq, seed, resume=None, logging_freq=100):
         self.train_loader = train_loader
         self.valid_loader = valid_loader
         self.device = device
@@ -97,24 +117,41 @@ class Trainer:
         self.grad_norm_threshold = grad_norm_threshold
         self.save_last_step_freq = save_last_step_freq
         self.save_global_step_freq = save_global_step_freq
+        self.seed = seed
+        self.logging_freq = logging_freq
+        
         self.ckpt_manager = CheckpointManager(expdir, "valid_acc")
         self.metrics = defaultdict(list)
-        self.step = 0
 
-        self.total_steps = (len(train_loader) * max_epoch) / self.accum_grad
+        self.total_steps = int((len(train_loader) * max_epoch) / self.accum_grad)
         self.optimizer = torch.optim.AdamW(model.parameters(), lr=2.5e-4)
         self.lr_scheduler = CosineScheduler(base_lr=2.5e-4, warmup_steps=10000, total_steps=self.total_steps)
-        
+
+        if resume:
+            ckpt_path = os.path.join(resume)
+            loaded = self.ckpt_manager.load_(ckpt_path, model, self.optimizer, self.lr_scheduler)
+            if loaded:
+                self.model, self.optimizer, self.lr_scheduler, self.step, self.metrics = loaded
+            else:
+                self.model = model.to(device)
+                self.step = 0
+        else:
+            self.model = model.to(device)
+            self.step = 0
+
+        self.batch = 0
+
         self.set_seed()
         
-    def set_seed(self, seed):
-        random.seed(seed)
-        np.random.seed(seed)
-        torch.manual_seed(seed)
-        torch.cuda.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed)
+    def set_seed(self):
+        random.seed(self.seed)
+        np.random.seed(self.seed)
+        torch.manual_seed(self.seed)
+        torch.cuda.manual_seed(self.seed)
+        torch.cuda.manual_seed_all(self.seed)
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
+
 
     def compute_grad_norm(self):
         norm_type = 2.0
@@ -125,46 +162,59 @@ class Trainer:
                 total_grad_norm += grad_norm
         return total_grad_norm ** (1 / norm_type)
 
+
     def train(self):
         self.model.to(self.device)
+        print(f"training started | epochs : {self.max_epoch} | batches : {len(self.train_loader)} | accum_grad : {self.accum_grad} | updates : {self.total_steps}")
         for epoch in range(self.max_epoch):
             self.train_epoch(epoch)
             self.validate_epoch(epoch)
-            if epoch % self.save_last_step_freq == 0:
-                self.ckpt_manager.save_checkpoint(self.model, self.optimizer, self.lr_scheduler, self.step, self.metrics, last_step=True)
+            
 
     def train_epoch(self, epoch):
         self.model.train()
-        epoch_train_loss, epoch_train_acc = 0, 0
+        avg_train_loss, avg_train_acc = 0, 0
         for i, batch in enumerate(self.train_loader):
             speech, speech_lengths, y = batch["speech"].to(self.device), batch["lengths"].to(self.device), batch["tokens"].to(self.device)
             logits, loss, acc = self.model(speech, speech_lengths, y)
-            epoch_train_loss += loss.item()
-            epoch_train_acc += acc
-            self.optimizer.zero_grad()
+            avg_train_loss += loss.item()
+            avg_train_acc += acc
             loss.backward()
-            grad_norm = self.compute_grad_norm()
-            torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.grad_norm_threshold)
-            lr = self.lr_scheduler.step()
-            for param_group in self.optimizer.param_groups:
-                param_group["lr"] = lr
-            self.optimizer.step()
-            self.step += 1
-            if self.step % self.save_global_step_freq == 0:
-                self.ckpt_manager.save_checkpoint(self.model, self.optimizer, self.lr_scheduler, self.step, self.metrics, last_step=False)
-            if self.step % self.save_last_step_freq == 0:
-                self.ckpt_manager.save_checkpoint(self.model, self.optimizer, self.lr_scheduler, self.step, self.metrics, last_step=True)
-            print(f"epoch {epoch}/{self.max_epoch} |  step {self.step}/{self.total_steps} |  loss {loss.item():.4f} | acc {acc:.4f} | grad Norm {grad_norm:.4f} | lR {lr}")
-
+            
+            if self.batch % self.accum_grad == 0:
+                grad_norm = self.compute_grad_norm()
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.grad_norm_threshold)
+                lr = self.lr_scheduler.step()
+                for param_group in self.optimizer.param_groups:
+                    param_group["lr"] = lr
+                self.optimizer.step()
+                self.step += 1
+                self.optimizer.zero_grad()
+                # save global step
+                if self.step % self.save_global_step_freq == 0:
+                    self.ckpt_manager.save_checkpoint(self.model, self.optimizer, self.lr_scheduler, self.step, self.metrics, last_step=False)
+                # svae last step
+                if self.step % self.save_last_step_freq == 0:
+                    self.metrics["train_loss"].append(avg_train_loss / self.save_last_step_freq)
+                    self.metrics["train_acc"].append(avg_train_acc / self.save_last_step_freq)
+                    avg_train_acc = 0
+                    avg_train_loss = 0
+                    self.ckpt_manager.save_checkpoint(self.model, self.optimizer, self.lr_scheduler, self.step, self.metrics, last_step=True)
+                # logging
+                if self.step % self.logging_freq == 0:
+                    print(f"epoch : {epoch}/{self.max_epoch} |  step : {self.step}/{self.total_steps} |  loss : {loss.item():.4f} | acc : {acc:.4f} | grad norm : {grad_norm:.4f} | lr : {lr}")
+            self.batch += 1
+                    
+                    
     def validate_epoch(self, epoch):
         self.model.eval()
-        epoch_valid_loss, epoch_valid_acc = 0, 0
+        avg_valid_loss, avg_valid_acc = 0, 0
         with torch.no_grad():
             for batch in self.valid_loader:
                 speech, speech_lengths, y = batch["speech"].to(self.device), batch["lengths"].to(self.device), batch["tokens"].to(self.device)
                 logits, loss, acc = self.model(speech, speech_lengths, y)
-                epoch_valid_loss += loss.item()
-                epoch_valid_acc += acc
-        self.metrics["valid_loss"].append(epoch_valid_loss / len(self.valid_loader))
-        self.metrics["valid_acc"].append(epoch_valid_acc / len(self.valid_loader))
-        print(f"Validation - epoch {epoch} | loss {epoch_valid_loss / len(self.valid_loader)} | acc {epoch_valid_acc / len(self.valid_loader)}")
+                avg_valid_loss += loss.item()
+                avg_valid_acc += acc
+        self.metrics["valid_loss"].append(avg_valid_loss / len(self.valid_loader))
+        self.metrics["valid_acc"].append(avg_valid_acc / len(self.valid_loader))
+        print(f"Validation - epoch {epoch} | loss {avg_valid_loss / len(self.valid_loader)} | acc {avg_valid_acc / len(self.valid_loader)}")
