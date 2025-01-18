926595eac9145e6d709986c8a544431fc52e3fb9
shoutrix <dasshoutrik@gmail.com>
2025-01-18 18:38:22 +0530

    added parameter initialization

--- E2asr_model.py
+++ E2asr_model.py
@@ -6,11 +6,12 @@ from dataclasses import dataclass
 import torch.nn.functional as F
 import numpy as np
 import sys
+import math
 
 @dataclass
 class ASRconfig:
     sample_rate: int = 16000
-    n_fft: int = 400
+    n_fft: int = 512
     win_length: int = 400
     hop_length: int = 160
     n_mels: int = 80
@@ -19,14 +20,16 @@ class ASRconfig:
     freq_mask_param: int = 15
     norm_mean: bool = True
     norm_var: bool = True
-    model_dim: int = 256
-    feedforward_dim: int = 1024
+    model_dim: int = 512
+    feedforward_dim: int = 2048
     dropout: float = 0.1
-    num_heads: int = 4
-    num_layers: int = 6
+    num_heads: int = 8
+    num_layers: int = 18
     encoder_normalize_first: bool = True
-    max_len: int = 5000
-
+    max_len: int = 4992
+    
+    def validate(self):
+        assert self.model_dim % self.num_heads == 0, f"model dim should be divisible by num_heads"
 
 
 class AudioFeatureExtractor(nn.Module):
@@ -42,13 +45,11 @@ class AudioFeatureExtractor(nn.Module):
             normalized=False,
             center=config.center
         )
-        self.log = lambda x: torch.log1p(x)
+        self.log = lambda x: torch.log(x + 1e-20)
     
     def forward(self, x, lengths):
         
         x = self.MelSpec(x) # shape : B, d, T
-        # print("upto here all good !!")
-        
 
         if self.config.center:
             frame_lengths = 1 + lengths // self.config.hop_length
@@ -62,7 +63,6 @@ class AudioFeatureExtractor(nn.Module):
         x = torch.where(mask, x, torch.zeros_like(x))
         x = self.log(x)
         return x, frame_lengths, mask
-
 
 
 class GlobalMVN(nn.Module):
@@ -109,13 +109,19 @@ class Conv2dSubsampling(nn.Module):
         super().__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(1, config.model_dim, 3, 2),
-            nn.ReLU(),
+            nn.GELU(),
+            nn.Dropout(p=0.1),
             nn.Conv2d(config.model_dim, config.model_dim, 3, 2),
-            nn.ReLU()
+            nn.GELU(),
+            nn.Dropout(p=0.1),
+            # nn.AvgPool2d(kernel_size=2)
         )
         in_dim = config.n_mels
         flattened_shape = (((in_dim - 1) // 2) - 1) // 2
-        self.linear = nn.Linear(flattened_shape * config.model_dim, config.model_dim)
+        self.linear = nn.Sequential(
+            nn.Dropout(p=0.1),
+            nn.Linear(flattened_shape * config.model_dim, config.model_dim)
+        )
 
     def forward(self, x):
         x = x.unsqueeze(1)
@@ -125,35 +131,67 @@ class Conv2dSubsampling(nn.Module):
         return x_feats
 
 
+
 class PositionWiseFeedForward(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.mod = nn.Sequential(
             nn.Linear(config.model_dim, config.feedforward_dim),
-            nn.ReLU(),
+            nn.GELU(),
             nn.Dropout(config.dropout),
             nn.Linear(config.feedforward_dim, config.model_dim)
         )
-    
     def forward(self, x):
         return self.mod(x)
 
 
+class MultiheadAttention(nn.Module):
+    def __init__(self, config):
+        super(MultiheadAttention, self).__init__()
+        self.config = config
+        self.q = nn.Linear(config.model_dim, config.model_dim)
+        self.k = nn.Linear(config.model_dim, config.model_dim)
+        self.v = nn.Linear(config.model_dim, config.model_dim)
+
+    def forward(self, x):
+        B, T, _ = x.shape
+        
+        querry = self.q(x)
+        key = self.k(x)
+        value = self.v(x)
+
+        querry = querry.view(B, T, self.config.num_heads, self.config.model_dim // self.config.num_heads).permute(0, 2, 1, 3)
+        key = key.view(B, T, self.config.num_heads, self.config.model_dim // self.config.num_heads).permute(0, 2, 1, 3)
+        value = value.view(B, T, self.config.num_heads, self.config.model_dim // self.config.num_heads).permute(0, 2, 1, 3)
+        
+        # scale = 1 / math.sqrt(self.config.model_dim // self.config.num_heads)
+        # score = torch.matmul(querry, key.transpose(2,3)) * scale
+        # score = score.float()
+        # norm_score = F.softmax(score, dim=-1).to(querry.dtype)
+        # out = torch.matmul(norm_score, value)
+        
+        out = F.scaled_dot_product_attention(querry, key, value, dropout_p=0.2)
+        out = out.permute(0, 2, 1, 3).contiguous().view(B, T, -1)
+        return out
+        
 class TransformerEncoderLayer(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
-        self.self_attn = nn.MultiheadAttention(config.model_dim, config.num_heads, dropout=config.dropout)
+        self.self_attn = MultiheadAttention(config)
         self.feed_forward = PositionWiseFeedForward(config)
         self.norm1 = nn.LayerNorm(config.model_dim)
         self.norm2 = nn.LayerNorm(config.model_dim)
         self.dropout = nn.Dropout(config.dropout)
+        
+        # self.register_buffer("residual_scale", (self.config.num_layers*2)**-0.5)
+        # self.register_buffer("residual_scale", torch.ones(1))
+        self.residual_scale = nn.Parameter(torch.ones(1))
 
-    def forward(self, x):
-        x = self.norm1(x)
-        attn_out, _ = self.self_attn(x, x, x)
-        x = x + self.dropout(attn_out)
-        x = x + self.dropout(self.feed_forward(self.norm2(x)))
+    def forward(self, x):  
+        
+        x = x + self.residual_scale * self.self_attn(self.norm1(x))
+        x = x + self.residual_scale * self.feed_forward(self.norm2(x))
         return x
 
 
@@ -172,16 +210,20 @@ class TransformerEncoder(nn.Module):
         input_pos = input_pos.unsqueeze(0).expand(B, -1, -1)
         
         x = input_feats + input_pos
-        for layer in self.layers:
+        for i, layer in enumerate(self.layers):
             x = layer(x)
+            x_mean = x.mean()
+            x_std = x.std()
+            print(f"encoder layer {i} | mean : {x_mean} | std : {x_std}")
         return self.norm(x)
 
-
 class E2ASR(nn.Module):
-    def __init__(self, config, vocab_size):
+    def __init__(self, config, vocab_size, training=True):
         super().__init__()
         self.config = config
         self.vocab_size = vocab_size
+        self.training = training
+        
         self.feature_extractor = AudioFeatureExtractor(config)
         self.specaug = SpecAugment(config)
         self.normalization = GlobalMVN(config)
@@ -189,24 +231,25 @@ class E2ASR(nn.Module):
         self.pred_head = nn.Linear(config.model_dim, self.vocab_size)
         
         self.loss_fn = nn.CrossEntropyLoss()
+        self.initialize_parameters()
 
     def forward(self, speech, speech_lengths, y):
-
         feats, frame_lengths, padding_mask = self.feature_extractor(speech, speech_lengths)
-        feats = self.specaug(feats)
-        feats = feats.transpose(1,2) # B,d,T -> B,T,d
-        feats = self.normalization(feats, frame_lengths, padding_mask)           
+        if self.training:
+            feats = self.specaug(feats)
+        feats = feats.transpose(1, 2)  # B,d,T -> B,T,d
+        feats = self.normalization(feats, frame_lengths, padding_mask)    
+        print(f"features scale | mean : {feats.mean()} | std : {feats.std()}")       
         out_ = self.encoder(feats)
         logits = self.pred_head(out_)
         
-        loss, acc = self.compute_cross_entropy_loss_and_acc(logits, y)
+        loss, acc = self.compute_masked_cross_entropy_loss_and_acc(logits, y)
         return logits, loss, acc
     
-
-    def compute_cross_entropy_loss_and_acc(self, logits, y):
-        y = y + 1 # to make 0 the filler token
+    def compute_masked_cross_entropy_loss_and_acc(self, logits, y):
+        y = y + 1  # to make 0 the filler token
         B, T, d = logits.shape
-        y = F.pad(y, (0, T  - y.shape[1]), value=0)
+        y = F.pad(y, (0, T - y.shape[1]), value=0)
         y_mask = y != 0
         y_mask = y_mask.view(-1)
             
@@ -220,6 +263,30 @@ class E2ASR(nn.Module):
         
         acc = correct_predicted / len(y_flat)
         return loss, acc
+
+    def initialize_parameters(self):
+        # encoder_PWFF_layer_linear_std = 0.5 * (self.config.num_layers * 2)**-0.5
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+                m.weight.data.mul_(0.1)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                m.weight.data.mul_(0.1)
+                # nn.init.normal_(m.weight, mean=0, std=0.02)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.LayerNorm):
+                nn.init.constant_(m.bias, 0)
+                nn.init.constant_(m.weight, 1)
+            elif isinstance(m, nn.Embedding):
+                nn.init.normal_(m.weight, mean=0, std=0.02)
+            
+        nn.init.xavier_uniform_(self.pred_head.weight)
+        nn.init.constant_(self.pred_head.bias, 0)
+
         
         
         
